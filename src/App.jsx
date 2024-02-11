381605b07eb141cc0f1b096614a25628afce0dab
Kris Kurzawa <kurzawa1@yahoo.com>
2024-02-11 18:35:58 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './index.css'
+import { IoHome } from "react-icons/io5";
+import Card from './components/card';
+import { useState } from 'react';
+import List from './components/list';
+
+const App = () => {
+  const [musicNumber, setMusicNumber] = useState(0);
+  const [open, setOpen] = useState(false)
+  return (
+    <div className='container'>
+      <div className="shape shape-1"></div>
+      <div className="shape shape-2"></div>
+      <div className="shape shape-3"></div>
+      <main>
+        <Card props={{ musicNumber, setMusicNumber, setOpen }} />
+        <List props={{ open, setOpen, musicNumber, setMusicNumber }} />
+      </main>
+    </div>
+  )
+}
+
+export default App
